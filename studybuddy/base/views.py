58475a8b0580d27f977d54a565ead7938269c9ac
Sean Anderson <84966214+seanandersxn@users.noreply.github.com>
2025-03-29 21:03:52 +0200

    Added Update Operation Functionality

--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -13,10 +13,12 @@ def home(request):
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
+
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
 
 def createRoom(request):
     form = RoomForm()
@@ -26,6 +28,20 @@ def createRoom(request):
        if form.is_valid():
            form.save()
            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
